2170c598bcbdfb64b7463339af5c0c1348d4115c
María Gabriela <Mara Gabriela>
2017-06-11 20:01:33 -0600

    Notifications Menu created without database relation.

---
+++ src/components/notifications/physicalActivityView.js
@@ -0,0 +1,42 @@
+'use strict'
+
+import React, { Component } from 'react';
+
+import { View, Text, StyleSheet, DatePickerAndroid, TouchableWithoutFeedback, TextInput, Alert } 
+from 'react-native';
+
+import { Container, Content, Header, Button, Title, Right, Left, Body, Icon, Form, Item,
+	Label, Input, Footer, InputGroup, Textarea, Toast } from 'native-base';
+
+export default class physicalActivityView extends Component {
+	
+	constructor(props) {
+        super(props);        
+    }
+
+	render() {
+		return (
+			<Container>
+
+				<Content style={StyleSheet.flatten(styles.contentStyle)}>
+					<Text>Physical Activity Notifications</Text>
+			</Content>
+		
+		</Container>
+			
+	);
+	}
+}
+
+const styles = StyleSheet.create({
+	contentStyle: {
+		padding: 20,
+        backgroundColor: '#F5FCFF',
+		paddingTop: 60
+	},
+	header: {
+		backgroundColor: '#009688'
+	}
+});
+
+module.exports = physicalActivityView;
