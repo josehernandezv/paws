f41a4426d34df073194d8c1a5dfdd22baf43aec6
josehernandezv <jose.andres.art@gmail.com>
2017-07-06 22:31:00 -0600

    Local notifications con react-native-push-notifications

--- android/app/src/main/java/com/paws/MainApplication.java
+++ android/app/src/main/java/com/paws/MainApplication.java
@@ -12,6 +12,7 @@ import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import com.magus.fblogin.FacebookLoginPackage; // <--- import
 import com.reactlibrary.googlesignin.RNGoogleSignInPackage;
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage; 
 
 import java.util.Arrays;
 import java.util.List;
@@ -32,6 +33,7 @@ public class MainApplication extends Application implements ReactApplication {
             new ImagePickerPackage(),
             new RNFetchBlobPackage(),
             new FacebookLoginPackage(),
+            new ReactNativePushNotificationPackage(),
            new RNGoogleSignInPackage() // <------ add the package
       );
     }
